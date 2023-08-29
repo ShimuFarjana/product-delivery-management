6364cf3a846e115b554a1a4209402398b1b1b076
ShimuFarjana <farjanashimu7@gmail.com>
2023-08-29 16:50:15 -0700

    add new

---
+++ ProductDeliveryManagement/ProductDeliveryManagement/Controllers/DeliveryPeopleController.cs
@@ -0,0 +1,64 @@
+﻿using ProductDeliveryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using X.PagedList;
+
+namespace ProductDeliveryManagement.Controllers
+{
+    public class DeliveryPeopleController : Controller
+    {
+        DeliveryManagementDbContext db = new DeliveryManagementDbContext();
+        // GET: DeliveryPeople
+        public ActionResult Index(int page=1)
+        {
+            return View(db.DeliveryPeople.OrderBy(x => x.DeliveryPersonId).ToPagedList(page, 5));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(DeliveryPerson person)
+        {
+            if(ModelState.IsValid)
+            {
+                db.DeliveryPeople.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+        public ActionResult Edit(int id)
+        {
+            var data = db.DeliveryPeople.FirstOrDefault(d => d.DeliveryPersonId == id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(DeliveryPerson person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = db.DeliveryPeople.FirstOrDefault(d => d.DeliveryPersonId == id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Delete(DeliveryPerson person)
+        {
+
+            db.Entry(person).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProductDeliveryManagement/ProductDeliveryManagement/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using ProductDeliveryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using X.PagedList;
+using X.PagedList.Mvc;
+
+
+namespace ProductDeliveryManagement.Controllers
+{
+    public class ProductsController : Controller
+    {
+        DeliveryManagementDbContext db = new DeliveryManagementDbContext();
+        // GET: Products
+        public ActionResult Index(int page=1)
+        {
+            return View(db.Products.OrderBy(x=>x.ProductId).ToPagedList(page, 5));
+        }
+        public ActionResult Create() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public ActionResult Edit(int id)
+        {
+            var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public ActionResult Delete(int id)
+        {
+            var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Delete(Product product)
+        {
+            db.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
